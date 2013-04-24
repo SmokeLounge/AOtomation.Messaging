b47255a9660babf56b7702c4f784cefc1a696693
bitterskittles <basementk1tt3h@gmail.com>
2013-04-24 16:40:13 +0100

    Added GameTimeMessage.

---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/GameTimeMessage.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameTimeMessage.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the GameTimeMessage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.GameTime)]
+    public class GameTimeMessage : N3Message
+    {
+        #region Constructors and Destructors
+
+        public GameTimeMessage()
+        {
+            this.N3MessageType = N3MessageType.GameTime;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [AoMember(0)]
+        public float Unknown1 { get; set; }
+
+        [AoMember(1)]
+        public int Unknown2 { get; set; }
+
+        [AoMember(2)]
+        public int Unknown3 { get; set; }
+
+        [AoMember(3)]
+        public float Unknown4 { get; set; }
+
+        #endregion
+    }
+}
