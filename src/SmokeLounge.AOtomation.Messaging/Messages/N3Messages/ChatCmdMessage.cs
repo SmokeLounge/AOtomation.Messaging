a0fa70cdeaae6363dc2bf1833d599f806bca0da0
bitterskittles <basementk1tt3h@gmail.com>
2013-04-26 18:07:55 +0100

    Added ChatCmdMessage.

---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/ChatCmdMessage.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ChatCmdMessage.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the ChatCmdMessage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.ChatCmd)]
+    public class ChatCmdMessage : N3Message
+    {
+        #region Constructors and Destructors
+
+        public ChatCmdMessage()
+        {
+            this.N3MessageType = N3MessageType.ChatCmd;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [AoMember(2, SerializeSize = ArraySizeType.Int32)]
+        public string Command { get; set; }
+
+        [AoMember(1)]
+        public Identity Target { get; set; }
+
+        [AoMember(0)]
+        public int Unknown1 { get; set; }
+
+        #endregion
+    }
+}
