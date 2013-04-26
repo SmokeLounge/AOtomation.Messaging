6d1cebd7f6437f052e5c4d1cc46d818c542385a7
bitterskittles <basementk1tt3h@gmail.com>
2013-04-26 09:35:10 +0100

    Added FollowTargetMessage.

---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/FollowTargetMessage.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FollowTargetMessage.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the FollowTargetMessage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.FollowTarget)]
+    public class FollowTargetMessage : N3Message
+    {
+        #region Constructors and Destructors
+
+        public FollowTargetMessage()
+        {
+            this.N3MessageType = N3MessageType.FollowTarget;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [AoMember(1)]
+        public Identity Target { get; set; }
+
+        [AoMember(0)]
+        public byte Unknown1 { get; set; }
+
+        [AoMember(2)]
+        public int Unknown2 { get; set; }
+
+        [AoMember(3)]
+        public int Unknown3 { get; set; }
+
+        [AoMember(4)]
+        public int Unknown4 { get; set; }
+
+        [AoMember(5)]
+        public int Unknown5 { get; set; }
+
+        [AoMember(6)]
+        public byte Unknown6 { get; set; }
+
+        #endregion
+    }
+}
