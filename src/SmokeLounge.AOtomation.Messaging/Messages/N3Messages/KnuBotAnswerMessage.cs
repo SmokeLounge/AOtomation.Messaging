5c29f16e8bb2e64501536af2f76f3a1603463fc7
bitterskittles <basementk1tt3h@gmail.com>
2013-04-26 08:57:37 +0100

    Added KnuBotAnswerMessage.

---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/KnuBotAnswerMessage.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KnuBotAnswerMessage.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the KnuBotAnswerMessage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.KnuBotAnswer)]
+    public class KnuBotAnswerMessage : N3Message
+    {
+        #region Constructors and Destructors
+
+        public KnuBotAnswerMessage()
+        {
+            this.N3MessageType = N3MessageType.KnuBotAnswer;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [AoMember(1)]
+        public Identity Target { get; set; }
+
+        [AoMember(0)]
+        public short Unknown1 { get; set; }
+
+        [AoMember(2)]
+        public int Unknown2 { get; set; }
+
+        #endregion
+    }
+}
