14818416bae65ed6b908008a4685e2d3eb1182a3
bitterskittles <basementk1tt3h@gmail.com>
2013-04-26 09:06:50 +0100

    Added LookAtMessage. Renamed an unknown in KnuBotAnswerMessage.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("SmokeLounge")]
 [assembly: AssemblyCopyright("Copyright © SmokeLounge 2013")]
-[assembly: AssemblyVersion("0.10.2.0")]
-[assembly: AssemblyFileVersion("0.10.2.0")]
+[assembly: AssemblyVersion("0.11.0.0")]
+[assembly: AssemblyFileVersion("0.11.0.0")]
--- src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/LookAtMessage.cs
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/LookAtMessage.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="KnuBotAnswerMessage.cs" company="SmokeLounge">
+// <copyright file="LookAtMessage.cs" company="SmokeLounge">
 //   Copyright © 2013 SmokeLounge.
 //   This program is free software. It comes without any warranty, to
 //   the extent permitted by applicable law. You can redistribute it
@@ -8,7 +8,7 @@
 //   http://www.wtfpl.net/ for more details.
 // </copyright>
 // <summary>
-//   Defines the KnuBotAnswerMessage type.
+//   Defines the LookAtMessage type.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -17,28 +17,22 @@ namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
     using SmokeLounge.AOtomation.Messaging.GameData;
     using SmokeLounge.AOtomation.Messaging.Serialization;
 
-    [AoContract((int)N3MessageType.KnuBotAnswer)]
-    public class KnuBotAnswerMessage : N3Message
+    [AoContract((int)N3MessageType.LookAt)]
+    public class LookAtMessage : N3Message
     {
         #region Constructors and Destructors
 
-        public KnuBotAnswerMessage()
+        public LookAtMessage()
         {
-            this.N3MessageType = N3MessageType.KnuBotAnswer;
+            this.N3MessageType = N3MessageType.LookAt;
         }
 
         #endregion
 
         #region Public Properties
 
-        [AoMember(2)]
-        public int Answer { get; set; }
-
-        [AoMember(1)]
-        public Identity Target { get; set; }
-
         [AoMember(0)]
-        public short Unknown1 { get; set; }
+        public Identity Target { get; set; }
 
         #endregion
     }
