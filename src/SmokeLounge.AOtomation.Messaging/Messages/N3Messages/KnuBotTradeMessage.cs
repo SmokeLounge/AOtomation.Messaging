cb8d049dc329a57c37b3f5fcf05b4c76dcec7397
bitterskittles <basementk1tt3h@gmail.com>
2013-04-26 12:44:50 +0100

    Added KnuBotTradeMessage.

---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/KnuBotTradeMessage.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KnuBotTradeMessage.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the KnuBotTradeMessage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.KnuBotTrade)]
+    public class KnuBotTradeMessage : N3Message
+    {
+        #region Constructors and Destructors
+
+        public KnuBotTradeMessage()
+        {
+            this.N3MessageType = N3MessageType.KnuBotTrade;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [AoMember(5)]
+        public Identity Container { get; set; }
+
+        [AoMember(1)]
+        public Identity Target { get; set; }
+
+        [AoMember(0)]
+        public short Unknown1 { get; set; }
+
+        [AoMember(2)]
+        public int Unknown2 { get; set; }
+
+        [AoMember(3)]
+        public int Unknown3 { get; set; }
+
+        [AoMember(4)]
+        public int Unknown4 { get; set; }
+
+        #endregion
+    }
+}
