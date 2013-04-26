de0fb1f31cf357ef6677bbfb8995bec9388b844b
bitterskittles <basementk1tt3h@gmail.com>
2013-04-26 14:58:01 +0100

    Added GenericCmdMessage.

--- src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/GenericCmdAction.cs
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/GenericCmdAction.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CommonAssemblyInfo.cs" company="SmokeLounge">
+// <copyright file="GenericCmdAction.cs" company="SmokeLounge">
 //   Copyright © 2013 SmokeLounge.
 //   This program is free software. It comes without any warranty, to
 //   the extent permitted by applicable law. You can redistribute it
@@ -8,15 +8,24 @@
 //   http://www.wtfpl.net/ for more details.
 // </copyright>
 // <summary>
-//   CommonAssemblyInfo.cs
+//   Defines the GenericCmdAction type.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    public enum GenericCmdAction
+    {
+        None = 0, 
 
+        Get = 1, 
 
-using System.Reflection;
+        Drop = 2, 
 
-[assembly: AssemblyCompany("SmokeLounge")]
-[assembly: AssemblyCopyright("Copyright © SmokeLounge 2013")]
-[assembly: AssemblyVersion("0.20.0.0")]
-[assembly: AssemblyFileVersion("0.20.0.0")]
+        Use = 3, 
+
+        Repair = 4, 
+
+        UseItemOnItem = 5
+    }
+}
---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/GenericCmdMessage.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GenericCmdMessage.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the GenericCmdMessage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.GenericCmd)]
+    public class GenericCmdMessage : N3Message
+    {
+        #region Constructors and Destructors
+
+        public GenericCmdMessage()
+        {
+            this.N3MessageType = N3MessageType.GenericCmd;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [AoMember(2)]
+        public GenericCmdAction Action { get; set; }
+
+        [AoMember(1)]
+        public int Count { get; set; }
+
+        [AoMember(5)]
+        public Identity Target { get; set; }
+
+        [AoMember(0)]
+        public int Temp1 { get; set; }
+
+        [AoMember(3)]
+        public int Temp4 { get; set; }
+
+        [AoMember(4)]
+        public Identity User { get; set; }
+
+        #endregion
+    }
+}
