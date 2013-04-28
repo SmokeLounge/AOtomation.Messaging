69162ec351b80eb76ab4b72d895a07e1b3040258
bitterskittles <basementk1tt3h@gmail.com>
2013-04-28 09:25:04 +0100

    Added BankMessage.

---
+++ src/SmokeLounge.AOtomation.Messaging/GameData/BankSlot.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BankSlot.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the BankSlot type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.GameData
+{
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    public class BankSlot
+    {
+        #region Public Properties
+
+        [AoMember(2)]
+        public short Count { get; set; }
+
+        [AoMember(1)]
+        public short Flags { get; set; }
+
+        [AoMember(3)]
+        public Identity Identity { get; set; }
+
+        [AoMember(0)]
+        public int ItemFlags { get; set; }
+
+        [AoMember(5)]
+        public int ItemHighId { get; set; }
+
+        [AoMember(4)]
+        public int ItemLowId { get; set; }
+
+        [AoMember(6)]
+        public int Quality { get; set; }
+
+        [AoMember(7)]
+        public int Unknown { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/BankMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.GameData;
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.Bank)]
+    public class BankMessage : N3Message
+    {
+        public BankMessage()
+        {
+            this.N3MessageType = N3MessageType.Bank;
+        }
+
+        [AoMember(0)]
+        public int Unknown1 { get; set; }
+
+        [AoMember(1)]
+        public int Unknown2 { get; set; }
+
+        [AoMember(2)]
+        public int Unknown3 { get; set; }
+
+        [AoMember(3, SerializeSize = ArraySizeType.X3F1)]
+        public BankSlot[] BankSlots { get; set; }
+    }
+}
