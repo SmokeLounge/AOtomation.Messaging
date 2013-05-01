47e98f31cc841245ef1e220af8455752575bb09a
bitterskittles <basementk1tt3h@gmail.com>
2013-05-01 16:45:47 +0100

    Added OrgInfoPacketMessage.

---
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/OrgInfoPacketMessage.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OrgInfoPacketMessage.cs" company="SmokeLounge">
+//   Copyright © 2013 SmokeLounge.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the OrgInfoPacketMessage type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
+{
+    using SmokeLounge.AOtomation.Messaging.Serialization;
+
+    [AoContract((int)N3MessageType.OrgInfoPacket)]
+    public class OrgInfoPacketMessage : N3Message
+    {
+        #region Constructors and Destructors
+
+        public OrgInfoPacketMessage()
+        {
+            this.N3MessageType = N3MessageType.OrgInfoPacket;
+        }
+
+        #endregion
+
+        #region AoMember Properties
+
+        [AoMember(0, SerializeSize = ArraySizeType.Int16)]
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
